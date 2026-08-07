e73b4844b60fe5f6d436e492e3c763502f4443c0
Fabien Tschanz <fabien.tschanz@bluewin.ch>
2026-08-08 01:28:35 +0200

    Remove null coalescing syntax

--- Modules/Microsoft365DSC/DscResources/MSFT_SPOHubSite/MSFT_SPOHubSite.psm1
+++ Modules/Microsoft365DSC/DscResources/MSFT_SPOHubSite/MSFT_SPOHubSite.psm1
@@ -149,7 +149,7 @@ class SPOHubSite : M365DSCResourceBase
                 }
             }
 
-            if (-not [System.String]::IsNullOrEmpty($this.LogoUrl) -and -not $this.LogoUrl.StartsWith('http'))
+            if (-not [System.String]::IsNullOrEmpty($this.LogoUrl) -and $this.LogoUrl.StartsWith('http'))
             {
                 $configuredLogo = $hubSite.LogoUrl
             }
